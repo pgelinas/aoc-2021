b3981dde3068bcc0506fa1bfde20c6f147454dc4
Pascal Gélinas <pascal.gelinas@ticksmith.com>
2021-12-03 16:16:10 -0500

    Day 2 solution

---
+++ src/Day02.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var horizontal = 0
+        var depth = 0
+        input.forEach {
+            val command = it.split(' ')
+            val operand = command[1].toInt()
+            when (command[0]) {
+                "forward" -> horizontal += operand
+                "up" -> depth -= operand
+                "down" -> depth += operand
+            }
+        }
+        return horizontal * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+        input.forEach {
+            val command = it.split(' ')
+            val operand = command[1].toInt()
+            when (command[0]) {
+                "forward" -> {
+                    horizontal += operand
+                    depth += aim * operand
+                }
+                "up" -> aim -= operand
+                "down" -> aim += operand
+            }
+        }
+        return horizontal * depth
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    println(part1(testInput))
+    println(part2(testInput))
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+
