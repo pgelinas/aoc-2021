55f33d7366010e2ea8fcdade843e6b64c1ec2aa2
Pascal Gélinas <pascal.gelinas@ticksmith.com>
2021-12-03 08:51:44 -0500

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var previous = Int.MAX_VALUE // ensure no previous measurement
+        input.forEach { depth: String ->
+            val current = depth.toInt()
+            if (current > previous) sum++
+            previous = current
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val window = ArrayDeque(input.slice(0..2).map(String::toInt))
+        var previous = window.sum()
+        val it = input.listIterator(3)
+        it.forEachRemaining {
+            window.removeFirst()
+            window.add(it.toInt())
+            val current = window.sum()
+            if (current > previous) sum++
+            previous = current
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = listOf("199", "200", "208", "210", "200", "207", "240", "269", "260", "263")
+    println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
